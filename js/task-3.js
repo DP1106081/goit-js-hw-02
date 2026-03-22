e6848ac152d7d9f8e24bbfc4f407d12225c384e1
DP <dpkds161@gmail.com>
2026-03-22 23:17:42 +0200

    hw02

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,17 @@
-function getElementWidth(content, padding, border) {
-  content = Number.parseFloat(content);
-  padding = Number.parseFloat(padding) * 2;
-  border = Number.parseFloat(border) * 2;
-  const totalWidth = content + padding + border;
-
-  console.log(totalWidth);
-  return totalWidth;
+function checkForSpam(message) {
+  const i_1 = 'spam';
+  const i_2 = 'sale';
+  const normal = message.toLowerCase();
+  if (normal.includes(i_1) || normal.includes(i_2)) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
